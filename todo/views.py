8da735ffa707b3eac8033b1f565a68f46d16293c
Dawit Abate <dawitabate2@gmail.com>
2021-11-06 13:20:45 +0300

    Added tasks page

--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Task
+from .forms import TaskForm
 
 # Create your views here.
+def index(request):
+    tasks_list = Task.objects.order_by('date_created')
+    if request.method == "POST":
+        data = request.POST
+        task_form = TaskForm(data)
+        if task_form.is_valid():
+            task_form.save()
+            redirect('tasks')
+    task_form = TaskForm()
+    context = {
+        "form": task_form,
+        "tasks": tasks_list,
+    }
+    return render(request, 'todo/index.html', context)
+    
